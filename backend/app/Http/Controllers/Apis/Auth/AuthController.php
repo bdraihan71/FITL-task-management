07884b812602dc3fe46b6c14450186c47c5b9883
bdraihan71 <bdraihan71@gmail.com>
2023-08-13 10:41:51 +0600

    Create auth api and controller

---
+++ backend/app/Http/Controllers/Apis/Auth/AuthController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Apis\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function register()
+    {
+
+    }
+
+    public function login()
+    {
+        
+    }
+
+    public function logout()
+    {
+        
+    }
+}
