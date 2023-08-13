8ea9bf39c6b5acdca9d67216a54c91897453e79c
bdraihan71 <bdraihan71@gmail.com>
2023-08-13 11:04:00 +0600

    Add storeUserRequest and register method on authController

--- backend/app/Http/Controllers/Apis/Auth/AuthController.php
+++ backend/app/Http/Controllers/Apis/Auth/AuthController.php
@@ -3,13 +3,29 @@
 namespace App\Http\Controllers\Apis\Auth;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 
 class AuthController extends Controller
 {
-    public function register()
+    public function register(StoreUserRequest $request)
     {
-
+        try{
+            $request['password']=Hash::make($request['password']);  //create new user
+            $user = User::create($request->toArray());  //create token
+            $token = $user->createToken('RaihanFarhad')->accessToken;
+            $data = [
+                        'user' => $user,
+                        'token' => $token
+                    ];
+            return $data;
+        
+        }catch(\Exception $exception){
+            Log::error("registration error : " . json_encode($exception->getMessage()) . " trace : " . json_encode($exception->getTrace()));
+            return $exception;
+        }
     }
 
     public function login()
